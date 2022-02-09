cf6f2a422d1a37f0b2a04570e9424aa515964f50
unknown <mac991@gmail.com>
2022-02-08 19:49:31 -0800

    implemented modify course updates

--- CourseRegistration/src/main/java/com/revature/course_registration/menus/ModifyCourseMenu.java
+++ CourseRegistration/src/main/java/com/revature/course_registration/menus/ModifyCourseMenu.java
@@ -2,21 +2,75 @@ package com.revature.course_registration.menus;
 
 import java.io.BufferedReader;
 
+import com.revature.course_registration.models.Course;
 import com.revature.course_registration.services.CourseService;
 import com.revature.course_registration.services.UserService;
 import com.revature.course_registration.util.MenuRouter;
+import com.revature.course_registration.util.collections.List;
+import com.revature.course_registration.util.logging.Logger;
 
 public class ModifyCourseMenu extends Menu {
+	private final UserService userService;
+	private final CourseService courseService;
+	private final Logger logger;
 
-	public ModifyCourseMenu(BufferedReader consoleReader, MenuRouter router,
-			UserService userService, CourseService courseService) {
+	public ModifyCourseMenu(BufferedReader consoleReader, MenuRouter router, UserService userService,
+			CourseService courseService) {
 		super("Modify Course Menu", "/modify-course-menu", consoleReader, router);
-		// TODO Auto-generated constructor stub
+		this.courseService = courseService;
+		this.userService = userService;
+		this.logger = Logger.getLogger(true);
 	}
 
 	@Override
 	public void render() throws Exception {
-		// TODO Auto-generated method stub
+
+		System.out.print("Enter course number: ");
+		String courseId = consoleReader.readLine();
+
+		String menu = "1) Change Course Name\n" + "2) Change Course Description\n" + "3) Change Course Maximum Seats\n"
+				+ "4) <<Back\n" + "> ";
+
+		System.out.print(menu);
+
+		String userSelection = consoleReader.readLine();
+		String courseData;
+		Course updatedCourse = new Course();
+
+		switch (userSelection) {
+		case "1":
+			System.out.print("New course name: ");
+			courseData = consoleReader.readLine();
+			updatedCourse = courseService.findCourseByID(courseId);
+			updatedCourse.setCourseName(courseData);
+			courseService.updateCourse(updatedCourse);
+			logger.log("Course name updated.");
+			router.transfer("/faculty-course-menu");
+			break;
+		case "2":
+			System.out.print("New course description: ");
+			courseData = consoleReader.readLine();
+			updatedCourse = courseService.findCourseByID(courseId);
+			updatedCourse.setCourseDescription(courseData);
+			courseService.updateCourse(updatedCourse);
+			logger.log("Course description updated.");
+			router.transfer("/faculty-course-menu");
+			break;
+		case "3":
+			System.out.print("New course maximum seats: ");
+			courseData = consoleReader.readLine();
+			updatedCourse = courseService.findCourseByID(courseId);
+			updatedCourse.setCourseSeatsMAX(Integer.parseInt(courseData));
+			courseService.updateCourse(updatedCourse);
+			logger.log("Course maximum seats updated.");
+			router.transfer("/faculty-course-menu");
+			break;
+		case "4":
+			router.transfer("/faculty-course-menu");
+			break;
+		default:
+			System.out.println("The user made an invalid selection");
+		}
 
 	}
 
